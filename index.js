8eb4c2e05f6b8dbf240b780d5e301d62e042fc71
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-26 05:07:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json))
-  return fetch();
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
